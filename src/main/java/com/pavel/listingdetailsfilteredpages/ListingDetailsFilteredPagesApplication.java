866ec054722db9317b1429bf7c80bca4cf6d5574
Pavel <belpav@gmail.com>
2023-03-01 22:42:12 +0200

    init commit

---
+++ src/main/java/com/pavel/listingdetailsfilteredpages/ListingDetailsFilteredPagesApplication.java
@@ -0,0 +1,13 @@
+package com.pavel.listingdetailsfilteredpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingDetailsFilteredPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingDetailsFilteredPagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavel/listingdetailsfilteredpages/ListingDetailsFilteredPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavel.listingdetailsfilteredpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingDetailsFilteredPagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
